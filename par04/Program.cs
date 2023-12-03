8d9b67c5b28001ccf53017120579ac939984d3e6
SDSOFIK <sdsofik2002@gmail.com>
2023-12-03 14:00:29 +0600

    my frist C#

---
+++ par04/Program.cs
@@ -0,0 +1,52 @@
+﻿using par04;
+
+//class
+/* student studentData = new student();
+studentData.name = "SD Sofik";
+studentData.Class = 12;
+studentData.roll = 3;
+studentData.id =246;
+Console.WriteLine("Your name :" +studentData.name);
+Console.WriteLine("class :"+ studentData.Class);
+Console.WriteLine( "id no:" + studentData.id);
+Console.WriteLine("roll no:"+ studentData.roll);
+
+Console.WriteLine("\n");
+
+//constructor 
+mayClas myData = new mayClas( "sofik" , 23, 2333);
+
+myData.showData();
+Console.WriteLine("\n");
+//staric
+Console.WriteLine(staric.pi);
+Console.WriteLine("\n");
+
+//staric constructor
+tacsoftSalary manun=new tacsoftSalary("manun" , 11,6000);
+manun.DisplaySalary();
+tacsoftSalary riyaz = new tacsoftSalary(" riyaz", 12, 30000);
+riyaz.DisplaySalary();
+Console.WriteLine("\n");
+
+
+child info = new child();
+info.name = "sofik";
+info.id = 1;
+info.age = 21;
+Console.WriteLine("you are neme is :" + info.name);
+Console.WriteLine("you are id no :" + info.id);
+Console.WriteLine("you are:" + info.age  + " year old");
+Console.WriteLine("\n");*/
+
+input show= new input();
+
+    Console.Write("Enter your id no :");
+show.num = Convert.ToInt32(Console.ReadLine()) ;
+
+Console.WriteLine($"radesh er id holo : {show.num+2}" );
+
+
+
+
+Console.ReadKey();
---
+++ par04/par04clas.cs
@@ -0,0 +1,85 @@
+﻿using System.Threading.Channels;
+
+namespace par04;
+//class
+
+ public class student
+{
+    public string name ;
+    
+    public double Class ;
+    public double roll;
+    public double id;
+}
+
+
+ //constructor
+
+public class mayClas 
+{
+  public  string name;
+    
+   public int id;
+    public int salary;
+    public mayClas(string name, int id, int salary )
+    {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        
+    }
+
+     public void showData()
+    {
+        Console.WriteLine($"my name is : {name} \n my id on : {id} \n salary : {salary}");
+    }
+
+
+}
+
+
+//static
+
+public class staric()
+{
+    public static float pi = 3.1416f;
+
+}
+
+//static-constructor
+
+public class tacsoftSalary
+{
+    public string name;
+    public int id;
+    public float salary;
+    public static float bonus = 400.679f;
+
+    public tacsoftSalary(string name, int id, float salary)
+    {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void DisplaySalary()
+    {
+        Console.WriteLine($"Your name: {name}, Your id: {id}, Bonus: {bonus}, Salary: {salary + bonus}");
+    }
+}
+// inheritance 
+public class father
+{
+    public string name;
+    public int id;
+
+} 
+public class child : father
+{
+    public int age;
+}
+
+public class input
+{
+public int num; 
+}
