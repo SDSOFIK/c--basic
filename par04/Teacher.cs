3b634f275f2a262e7711b90fa159a99dc3683b20
SDSOFIK <sdsofik2002@gmail.com>
2023-12-03 14:12:53 +0600

    Add Teacher Class

---
+++ par04/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace par04;
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+   
+}
